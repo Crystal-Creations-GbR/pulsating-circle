5ae491e4dacd37e27cb9b1921610b3dcfe390894
Johannes Huther <dev.johannes@huther.link>
2023-07-28 21:32:27 +0200

    ci: set up release workflows on next
    Run semantic-release workflow for pushes to branch `next`.

--- release.config.js
+++ release.config.js
@@ -51,8 +51,8 @@ module.exports = {
   branches: [
     "+([0-9])?(.{+([0-9]),x}).x",
     "main",
-    "next",
-    "next-major",
+    { name: "next", prerelease: true },
+    { name: "next-major", prerelease: true },
     { name: "beta", prerelease: true },
     { name: "alpha", prerelease: true },
   ],
