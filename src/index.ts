Crystal-Creations-GbR/animated-checkmark

build: upgrade to vue 3
Upgrade module to vue 3 and use vite as the build tool.

BREAKING CHANGE: imports changed

The importing process for this package has changed slightly in vue 3. Refer to `README.md` section "Usage".

Closes #35

src/index.ts
@@ -1,3 +1,3 @@
-import AnimatedCheckmark from "./AnimatedCheckmark.vue";
+import AnimatedCheckmark from "./components/AnimatedCheckmark.vue";
 
 export { AnimatedCheckmark };

vite.config.ts
@@ -1,0 +1,24 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "node:path";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), dts()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "AnimatedCheckmark",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});

Crystal-Creations-GbR/pulsating-circle

src/index.ts
@@ -1,3 +1,3 @@
-import PulsatingCircle from "./PulsatingCircle.vue";
+import PulsatingCircle from "./components/PulsatingCircle.vue";
 
 export { PulsatingCircle };

vite.config.ts
@@ -1,0 +1,24 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), dts()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "PulsatingCircle",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});